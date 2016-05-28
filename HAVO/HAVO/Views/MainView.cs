bb37db18f36da8c53611bb9d41fec2e41745b971
jorge <jorgeafs99@gmail.com>
2016-05-28 12:12:53 +0200

    begin ui

---
+++ HAVO/HAVO/Views/MainView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HAVO.Views
+{
+	public class MainView : ContentPage
+	{
+		public MainView ()
+		{
+            String title = "Menu Principal";
+            String listaNueva = "Nueva lista";
+            String listaCargar = "Cargar lista";
+            String exit = "Salir";
+            this.SetBinding(ContentPage.TitleProperty, title);
+
+            var nuevaLista = new Button { Text = listaNueva };
+            nuevaLista.Clicked
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
