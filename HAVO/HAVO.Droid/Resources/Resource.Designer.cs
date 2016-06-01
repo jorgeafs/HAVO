7b986fd01f409b84b9ae0e6a0dda7344ebe93465
jorge <jorgeafs99@gmail.com>
2016-06-01 19:13:55 +0200

    check
    added check images

--- HAVO/HAVO.Droid/Resources/Resource.Designer.cs
+++ HAVO/HAVO.Droid/Resources/Resource.Designer.cs
@@ -1503,7 +1503,13 @@ namespace HAVO.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int check = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int check2x = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
 			
 			static Drawable()
 			{
